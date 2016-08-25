9520e54487a85040a606f6ac84260794b37755df
Sathish Kumar Parthasarathy <Sathish Kumar Parthasarathy>
2016-08-24 21:46:15 -0500

    print the record set

--- src/main/scala/org/hello/HelloScalaOracleJDBC.scala
+++ src/main/scala/org/hello/HelloScalaOracleJDBC.scala
@@ -17,6 +17,16 @@ object HelloScalaOracleJDBC {
       connection = DriverManager.getConnection(url, userName, password);
       var statement = connection.createStatement();
       var resultSet = statement.executeQuery(sqlQuery);
+      while (resultSet.next()){
+        print(resultSet.getString(1) + "\t")
+        print(resultSet.getString(2) + "\t")
+        print(resultSet.getString(3) + "\t")
+        print(resultSet.getString(4) + "\t")
+        print(resultSet.getString(5) + "\t")
+        print(resultSet.getString(6) + "\t")
+        print(resultSet.getString(7) + "\t")
+        print(resultSet.getString(8) + "\n")
+      }
       
     } catch {
       case t: Throwable => t.printStackTrace() // TODO: handle error
